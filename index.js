53e984a75b9a0f1b6aa9ad953b6774dfa2357a6d
ZeroByter <romgamesrom@gmail.com>
2021-11-19 12:28:16 +0200

    fixed things

--- index.js
+++ index.js
@@ -19,56 +19,54 @@ function generateMemes(data) {
     }
 }
 
-document.body.onload = function () {
-    generateMemes([
-        {
-            url: "2d-excavator/index",
-            name: "2d-excavator",
-            desc: "2d excavator simulator... thing..."
-        },
-        {
-            name: "bring resources",
-            desc: "Meant to be a thing showing robots carrying things, inspired by the <del>slaves</del>drones in Factorio"
-        },
-        {
-            name: "closest point",
-            desc: "click on the screen to create points ( ͡° ͜ʖ ͡°)"
-        },
-        {
-            name: "color circles",
-            desc: "Sort of inspired by color lines, but it's a circle. Press W/S to change modes, left/right arrows to change offset, up/down arrows to change line thickness"
-        },
-        {
-            name: "color lines",
-            desc: "It's a line, it's colorful, it moves"
-        },
-        {
-            name: "color screen",
-            desc: "Drawing a bunch of colorful lines on a screen without clearing it"
-        },
-        {
-            name: "funky",
-            desc: "Just something funky"
-        },
-        {
-            name: "influence copy",
-            desc: "Trying to create a copy of the mobile Influence game, abandoned..."
-        },
-        {
-            name: "jumpy",
-            desc: "Click on the screen to create points, a red boi will jump across all over them."
-        },
-        {
-            name: "lotta explodey",
-            desc: "Was meant to be a clone of an old Java meme I made, kinda weird"
-        },
-        {
-            name: "power",
-            desc: "Testing arcade power distribution across nodes (Factorio inspired)"
-        },
-        {
-            name: "random-test",
-            desc: "It's not a random test, it's a literal test of randomness. Trying to test different types of random occurance methods"
-        },
-    ])
-}
+generateMemes([
+    {
+        url: "2d-excavator/index",
+        name: "2d-excavator",
+        desc: "2d excavator simulator... thing..."
+    },
+    {
+        name: "bring resources",
+        desc: "Meant to be a thing showing robots carrying things, inspired by the <del>slaves</del>drones in Factorio. Press number keys 1-3 to change tool"
+    },
+    {
+        name: "closest point",
+        desc: "click on the screen to create points ( ͡° ͜ʖ ͡°)"
+    },
+    {
+        name: "color circles",
+        desc: "Sort of inspired by color lines, but it's a circle. Press W/S to change modes, left/right arrows to change offset, up/down arrows to change line thickness"
+    },
+    {
+        name: "color lines",
+        desc: "It's a line, it's colorful, it moves"
+    },
+    {
+        name: "color screen",
+        desc: "Drawing a bunch of colorful lines on a screen without clearing it"
+    },
+    {
+        name: "funky",
+        desc: "Just something funky"
+    },
+    {
+        name: "influence copy",
+        desc: "Trying to create a copy of the mobile Influence game, abandoned..."
+    },
+    {
+        name: "jumpy",
+        desc: "Click on the screen to create points, a red boi will jump across all over them."
+    },
+    {
+        name: "lotta explodey",
+        desc: "Was meant to be a clone of an old Java meme I made, kinda weird"
+    },
+    {
+        name: "power",
+        desc: "Testing arcade power distribution across nodes (Factorio inspired)"
+    },
+    {
+        name: "random-test",
+        desc: "It's not a random test, it's a literal test of randomness. Trying to test different types of random occurance methods"
+    },
+])
--- meme-template.js
+++ meme-template.js
@@ -1,19 +1,28 @@
-document.body.onload = function () {
-    document.body.insertAdjacentHTML("afterbegin", `<link rel="stylesheet" href="/meme-template.css">`)
+function repeatString(str, times) {
+    let result = ""
 
-    document.body.insertAdjacentHTML("beforeend", `<a href="/"><button class="go-back">go back</button></a>`)
+    for (let i = 0; i < times; i++) {
+        result += str
+    }
 
-    const goBackButton = document.querySelector(".go-back")
-    const hideShowElements = [goBackButton]
-    let elementsVisible = true
-
-    document.body.addEventListener("keydown", function (e) {
-        if(e.code == "F1"){
-            elementsVisible = !elementsVisible
-            for(const element of hideShowElements){
-                element.style.display = elementsVisible ? null : "none"
-            }
-            e.preventDefault()
-        }
-    })
+    return result
 }
+
+//repeat string is used here in case we want to include this css somewhere not in root
+document.body.insertAdjacentHTML("afterbegin", `<link rel="stylesheet" href="${repeatString("../", window.location.href.length - 1)}meme-template.css">`)
+
+document.body.insertAdjacentHTML("beforeend", `<a href=".."><button class="go-back">go back</button></a>`)
+
+const goBackButton = document.querySelector(".go-back")
+const hideShowElements = [goBackButton]
+let elementsVisible = true
+
+document.body.addEventListener("keydown", function (e) {
+    if (e.code == "F1") {
+        elementsVisible = !elementsVisible
+        for (const element of hideShowElements) {
+            element.style.display = elementsVisible ? null : "none"
+        }
+        e.preventDefault()
+    }
+})
